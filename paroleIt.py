5279287eb3c3966ebaf6d733560a036e77056ae5
Tim Somov <str8tim@yandex.ru>
2023-02-28 17:24:57 +0500

    - Rewrote code for GUI - Added comments

--- paroleIt.py
+++ paroleIt.py
@@ -1,22 +1,54 @@
 import random
+from tkinter import *
 
-print("This is a password generator.")
+# Creating main class
+class Application(Frame):
+    def __init__(self, master):
+        super(Application, self).__init__(master)
+        self.grid()
+        self.create_widgets()
 
-new_parole_length = int(input("Enter a length of your new password: "))
+# Configure widgets module
+    def create_widgets(self):
+        self.inst_lbl = Label(self, text = "Choose a length of your new password")
+        self.inst_lbl.grid(row = 0, column = 0, columnspan = 2, sticky = W)
 
-if new_parole_length < 10:           
-    print("Your password will be too easy. Try again.")             
-elif new_parole_length > 15:        
-    print("It's not necessary to set up such long password. Try again.")        
-else:        
-    print("OK, let's do it!")
-            
-    password_length = 0
-    symbols = ""
-    while password_length != new_parole_length:
-        new_pass = random.randrange(33, 127)
-        new_pass = chr(new_pass)
-        symbols += new_pass
-        password_length +=1
-            
-    print("Your new password is: ", symbols)
+# Text field
+        self.pwlen_ent = Entry(self, width = 5)
+        self.pwlen_ent.grid(row = 0, column = 2, sticky = E)
+
+# Button
+        self.submit_bttn = Button(self, text = "Generate new password", command = self.reveal)
+        self.submit_bttn.grid(row = 1, column = 0, sticky = W)
+
+# Text field for generated password
+
+        self.pw_gentd = Entry(self, width = 0)
+        self.pw_gentd.grid(row = 2, column = 0, sticky = W)
+
+# Reveal module
+    def reveal(self):
+        contents = int(self.pwlen_ent.get())
+
+        if contents < 10:
+            message = "Your password will be too easy. Try again"
+        elif contents > 15:
+            message = "Not necessary to set up such long password. Try again"
+        else:
+            password_length = 0
+            symbols = ""
+            while password_length != contents:
+                new_password = random.randrange(33, 127)
+                new_password = chr(new_password)
+                symbols += new_password
+                password_length += 1
+            message = symbols
+
+        self.pw_gentd.delete(0, END)
+        self.pw_gentd.insert(0, message)
+
+# Main
+root = Tk()
+root.title("Parole It")
+app = Application(root)
+root.mainloop()
